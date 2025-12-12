c422b20669e8a3df2e5a7b1ff992c19250bb5109
Farha-Wael <F.Wael2492@nu.edu.eg>
2025-12-12 22:49:16 +0200

    Add project files.

---
+++ Delivery-Service/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delivery_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
